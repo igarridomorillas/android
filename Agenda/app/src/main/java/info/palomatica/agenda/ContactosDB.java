247b8582f87b15db5ee96ff54d2383d66b93081d
ProfeLuisVazquez <luisvazquez007@gmail.com>
2019-01-17 20:12:25 +0100

    jueves 17

---
+++ Agenda/app/src/main/java/info/palomatica/agenda/ContactosDB.java
@@ -0,0 +1,105 @@
+package info.palomatica.agenda;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class ContactosDB extends SQLiteOpenHelper
+{
+
+    private SQLiteDatabase db;
+
+    private static ContactosDB contactosDB = null;
+
+
+    public static ContactosDB getInstance(Context context)
+    {
+        if(contactosDB == null)
+        {
+            contactosDB = new ContactosDB(context);
+        }
+
+        return contactosDB;
+    }
+
+
+    private ContactosDB(Context context)
+    {
+        super(context, "contactos.db", null, 1);
+        db = getWritableDatabase();
+    }
+
+    public ContactosDB(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
+    {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL("CREATE TABLE contactos (_id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                                           "nombre TEXT," +
+                                           "telefono TEXT," +
+                                           "categoria INTEGER);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+
+    }
+
+    public boolean insertarContacto(Contacto contacto)
+    {
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("nombre", contacto.getNombre());
+        contentValues.put("telefono", contacto.getTelefono());
+        contentValues.put("categoria", contacto.getCategoria());
+
+        db.insert("contactos", null, contentValues);
+
+        /*
+        db.execSQL("INSERT INTO contactos (nombre, telefono, categoria) values " +
+                "('" + contacto.getNombre() + "','" + contacto.getTelefono() + "'," +
+                + contacto.getCategoria() + ");");
+        */
+        return true;
+    }
+
+    public ArrayList<Contacto> getContactos()
+    {
+        Cursor cursor = db.rawQuery("SELECT _id, nombre, telefono, categoria FROM contactos order by nombre;", null);
+        ArrayList<Contacto> alContactos = new ArrayList<>(cursor.getCount());
+
+        Contacto contacto;
+
+        if(cursor.moveToFirst())
+        {
+            do
+            {
+                contacto = new Contacto();
+
+                contacto.setId(cursor.getInt(0))
+                    .setNombre(cursor.getString(1))
+                    .setTelefono(cursor.getString(2))
+                    .setCategoria(cursor.getInt(3));
+
+                alContactos.add(contacto);
+            }
+            while (cursor.moveToNext());
+        }
+
+        return alContactos;
+    }
+
+    public void eliminarContacto(int id)
+    {
+        db.execSQL("delete from contactos where _id = " + id);
+    }
+}
--- Agenda/app/src/main/java/info/palomatica/agenda/MainActivity.java
+++ Agenda/app/src/main/java/info/palomatica/agenda/MainActivity.java
@@ -1,17 +1,19 @@
 package info.palomatica.agenda;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import java.util.zip.Inflater;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity
     private final int REQUEST_CODE_CREAR_CONTACTO = 0;
     private LinearLayout llContactos;
 
+    private ContactosDB contactosDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -27,29 +31,141 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         llContactos = findViewById(R.id.llContactos);
+
+        contactosDB = ContactosDB.getInstance(this);
+
+        dibujarContactos();
+
     }
+
+    private void dibujarContactos()
+    {
+        llContactos.removeAllViews();
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        RelativeLayout rlContacto;
+
+        for(final Contacto contacto: contactosDB.getContactos())
+        {
+            rlContacto = (RelativeLayout) layoutInflater.inflate(R.layout.item_contacto, null);
+            TextView tvNombre = rlContacto.findViewById(R.id.tvNombre);
+            TextView tvTelefono = rlContacto.findViewById(R.id.tvTelefono);
+            ImageView ivIcono = rlContacto.findViewById(R.id.ivIcono);
+
+            tvNombre.setText(contacto.getNombre());
+            tvTelefono.setText(contacto.getTelefono());
+
+            switch (contacto.getCategoria())
+            {
+                case Contacto.CAT_AMIGOS:
+                   ivIcono.setBackgroundResource(R.drawable.cat_amigos);
+                   break;
+                case Contacto.CAT_FAMILIARES:
+                    ivIcono.setBackgroundResource(R.drawable.cat_familia);
+                    break;
+                case Contacto.CAT_TRABAJO:
+                    ivIcono.setBackgroundResource(R.drawable.cat_trabajo);
+                    break;
+            }
+
+            rlContacto.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(final View v)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                    builder.setTitle(R.string.dialogoBorrarContacto)
+                        .setMessage(R.string.dialogoConfirmar)
+                        .setPositiveButton(R.string.dialogoSi, new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                contactosDB.eliminarContacto(contacto.getId());
+                                llContactos.removeView(v);
+                                dialog.dismiss();
+                            }
+                        });
+                    builder.setNegativeButton(R.string.dialogoNo, new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                    return true;
+                }
+            });
+
+            rlContacto.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    String[] opciones = { getString(R.string.dialogoLlamar), getString(R.string.dialogoEnviarSMS)};
+
+
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle(R.string.dialogoOpciones);
+
+                    builder.setItems(opciones, new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            if(which == 0) // Llamar
+                            {
+                                Intent intent = new Intent(Intent.ACTION_CALL);
+                                intent.setData(Uri.parse("tel:" + contacto.getTelefono()));
+
+                                startActivity(intent);
+                            }
+                            else if(which == 1) // SMS
+                            {
+
+                            }
+
+                            dialog.dismiss();
+                        }
+                    });
+
+
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            });
+
+
+            llContactos.addView(rlContacto);
+        }
+    }
+
 
     public void onClickNuevoContacto(View view)
     {
-        /*
+
+
         Intent intent = new Intent(this, CrearContactoActivity.class);
         startActivityForResult(intent, REQUEST_CODE_CREAR_CONTACTO);
-        */
-
-        LayoutInflater layoutInflater = getLayoutInflater();
-        RelativeLayout rlContacto = (RelativeLayout) layoutInflater.inflate(R.layout.item_contacto, null);
-
-        TextView tvNombre = rlContacto.findViewById(R.id.tvNombre);
-        tvNombre.setText("Pepe");
-        TextView tvTelefono = rlContacto.findViewById(R.id.tvTelefono);
-        tvTelefono.setText("3453453");
-
-        llContactos.addView(rlContacto);
-
-
 
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
 
+        if(resultCode == RESULT_OK && requestCode == REQUEST_CODE_CREAR_CONTACTO)
+        {
+            dibujarContactos();
+        }
+
+    }
 }
